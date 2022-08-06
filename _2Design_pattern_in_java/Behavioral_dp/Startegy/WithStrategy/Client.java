e7f8a47381e87aed0b7a319d5617d1cb33c2995e
Amol <includeamol@gmail.com>
2022-08-07 00:55:15 +0530

    Updated Startegy Pattern_2

--- _2Design_pattern_in_java/Behavioral_dp/Startegy/WithStrategy/Client.java
+++ _2Design_pattern_in_java/Behavioral_dp/Startegy/WithStrategy/Client.java
@@ -2,10 +2,15 @@ package _2Design_pattern_in_java.Behavioral_dp.Startegy.WithStrategy;
 
 public class Client {
     public static void main(String[] args) {
-        SportsBike spbike = new SportsBike();
-        SportsCar spcar = new SportsCar();
-        NormalBike nbike = new NormalBike();
-        NormalCar ncar = new NormalCar();
+        // SportsBike spbike = new SportsBike();
+        // SportsCar spcar = new SportsCar();
+        // NormalBike nbike = new NormalBike();
+        // NormalCar ncar = new NormalCar();
+
+        Vechile spbike = new SportsBike();
+        Vechile spcar = new SportsCar();
+        Vechile nbike = new NormalBike();
+        Vechile ncar = new NormalCar();
 
         spbike.drive();
         spcar.drive();
